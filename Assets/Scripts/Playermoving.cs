8b6871ffd1e7b37273421c55d38aee58dee8d4df
wshhaha <44226583+wshhaha@users.noreply.github.com>
2019-01-24 11:09:47 +0900

    Initial commit

---
+++ Assets/Scripts/Playermoving.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermoving : MonoBehaviour 
+{
+    public UIJoystick stick;
+    public float spd;
+    Vector3 stickpos;
+    private void Start()
+    {
+        this.enabled = false;
+    }
+    void Update () 
+	{
+        stickpos.x = stick.position.x;
+        stickpos.y = 0;
+        stickpos.z = stick.position.y;
+        Vector3 dir = stickpos - transform.position;
+        
+        dir.y = 0;
+        
+        dir.Normalize();
+        
+        transform.rotation = Quaternion.Lerp(transform.rotation,Quaternion.LookRotation(dir),10 * Time.deltaTime);
+    }
+   
+}
